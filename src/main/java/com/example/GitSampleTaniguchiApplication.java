f7756b0884c8722ea973da6ba322659a09881ba8
taniguchihatsune <hatsune.taniguchi@rakus-partners.co.jp>
2021-11-04 12:04:21 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleTaniguchiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleTaniguchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleTaniguchiApplication.class, args);
+	}
+
+}
